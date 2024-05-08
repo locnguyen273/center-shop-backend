cf6f4177affd296c1923e18a3ebb913b5e8ccab9
Loc Nguyen <tanlocit20062000@gmail.com>
2024-05-08 23:36:04 +0700

    .

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # center-shop-backend
+
+--- env ---
+PORT=5000 
+MONGODB_URL="mongodb+srv://locit2062000:jltGCTM8qJIWfcqJ@keyboard-db.2psskih.mongodb.net/keyboard" 
+JWT_SECRET="ZXCBNMLKJHGFDSAPOIUYTREWQmnbvcxzlkjhgfdsapoiuytrewq0987654321" 
+MAIL_ID=tanloc20600@gmail.com MP=tyxzsmipynuxuvvp
---
+++ src/index.js
@@ -0,0 +1,34 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const dbConnect = require("./configs/databaseConnect");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT;
+
+dbConnect();
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+// This should be the last route else any after it won't work
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: "false",
+    message: "Page not found",
+    error: {
+      statusCode: 404,
+      message: "You reached a route that is not defined on this server",
+    },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running  at PORT ${PORT}`);
+});
