490523d7cdea348c8c877770e07abe0e65dfd3d9
Loc Nguyen <nguyentanloc@teams.airlab.dev>
2024-05-08 18:16:18 +0700

    init repo

---
+++ src/configs/databaseConnect.js
@@ -0,0 +1,20 @@
+const { default: mongoose } = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const databaseConnect = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
+};
+
+module.exports = databaseConnect;
